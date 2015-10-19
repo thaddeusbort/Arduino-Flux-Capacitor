fded585aaf8417a8203352eaeba027959108f837
Thaddeus Bort <thaddeus.bort@ampelectricvehicles.com>
2015-10-19 01:07:01 -0400

    added some test code for a time travel mode that should run every 5 minutes  it turns the center and outer blue leds on at the end of the sequence and should progressively get faster

--- flux_capacitor.ino
+++ flux_capacitor.ino
@@ -1,34 +1,44 @@
-//#include <pcmConfig.h>
-//#include <pcmRF.h>
 #include <TMRpcm.h>
-
 #include <SD.h>
 #include <SPI.h>
-
 
 /*
   Flux Capacitor
   Lights up LEDs in a sequence to look like the flux capacitor.
 
-  October 16, 2015
+  Started October 16, 2015
   by Thaddeus Bort, Jr
  */
 
+// sd card pinout
+// CS 10
+// MOSI 11
+// MISO 12
+// SCK 13
 #define SD_CHIP_SELECT_PIN 10
 
 TMRpcm audio; // an object for playing sounds
 
-const long interval = 120;
+#define DEFAULT_INTERVAL 50
+long interval = DEFAULT_INTERVAL;
 const byte ledGroups[4] = { A0, A1, A2, A3 };
+const byte timeTravelLeds[2] = { A4, A5 };
 
 bool ledEnabled = true;
 unsigned long previousMillis = 0;        // will store last time LED was updated
 byte currentLedGroup = 0;
+bool currentState = false;
+
+unsigned long previousTimeTravelMillis = 0;
+bool timeTravelMode = false;
 
 void setup() {
   Serial.begin(9600);
   for(byte i=0; i<sizeof(ledGroups); i++) {
     pinMode(ledGroups[i], OUTPUT);
+  }
+  for(byte i=0; i<sizeof(timeTravelLeds); i++) {
+    pinMode(timeTravelLeds[i], OUTPUT);
   }
 
   // initialize
@@ -58,16 +68,35 @@ void loop() {
     if(!ledEnabled)
       turnAllLedsOff();
   }
+
+  if(!timeTravelMode && (currentMillis-previousTimeTravelMillis >= 300000)) {
+    timeTravelMode = true;
+  }
  
   if(ledEnabled && (currentMillis - previousMillis >= interval)) {
     // save the last time you blinked the LED 
     previousMillis = currentMillis;
 
-    // turn current led off and next led on
-    digitalWrite(ledGroups[currentLedGroup], LOW);
-    if(++currentLedGroup >= sizeof(ledGroups))
+    // cycle through leds, turning on and then off and then moving to next led
+    digitalWrite(ledGroups[currentLedGroup], currentState ? LOW : HIGH);
+    currentState = !currentState;
+    if(!currentState && ++currentLedGroup >= sizeof(ledGroups))
       currentLedGroup = 0;
-    digitalWrite(ledGroups[currentLedGroup], HIGH);
+
+    if(timeTravelMode) {
+      if(currentLedGroup == 3 && !currentState) {
+        digitalWrite(timeTravelLeds[0], HIGH);
+        digitalWrite(timeTravelLeds[1], HIGH);
+      } else {
+        digitalWrite(timeTravelLeds[0], LOW);
+        digitalWrite(timeTravelLeds[1], LOW);
+      }
+      if(--interval <= 0) {
+        previousTimeTravelMillis = currentMillis;
+        timeTravelMode = false;
+        interval = DEFAULT_INTERVAL;
+      }
+    }
   }
 }
 
@@ -76,6 +105,7 @@ void turnAllLedsOff() {
     digitalWrite(ledGroups[i], LOW);
   }
   currentLedGroup = 0;
+  currentState = false;
   previousMillis = 0;
 }
 
